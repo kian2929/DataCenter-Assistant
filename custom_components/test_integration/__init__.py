5daa3a594b46c99e3e081c0298454e555d9ad68c
Kian Farhadi <kian.farhadi@outlook.de>
2025-04-27 15:50:48 +0200

    Neuer Service run_test hinzugefügt

--- custom_components/test_integration/__init__.py
+++ custom_components/test_integration/__init__.py
@@ -1,9 +1,19 @@
-"""Init file for Test-Integration."""
-DOMAIN = "test_integration"
+import logging
+from homeassistant.core import HomeAssistant
 
-async def async_setup_entry(hass, config_entry):
-    """Set up Test Integration from a config entry."""
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(config_entry, "button")
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "test_integration"
+SERVICE_RUN = "run_test"
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the Test Integration."""
+    async def handle_run_test(call):
+        _LOGGER.info("Der Test-Service wurde aufgerufen und funktioniert!")
+
+    hass.services.async_register(
+        DOMAIN, SERVICE_RUN, handle_run_test
     )
+
+    _LOGGER.info("Test Integration geladen!")
     return True
